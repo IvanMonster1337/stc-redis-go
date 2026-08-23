d5af030b808a35295580573709c32b32b65463d0
Ivan <ivan.markevich329@gmail.com>
2026-08-23 13:13:47 +0300

    feat: PING cmd

--- main.go
+++ main.go
@@ -12,8 +12,11 @@ func handleCommand(args []string) string {
 
 	switch cmd {
 	case "PING":
-		// TODO: Return "+PONG\r\n" for no args
-		// TODO: Return bulk string for PING <message>
+		if len(args) > 1 {
+			return encodeBulkString(args[1])
+		}
+
+		return "+PONG\r\n"
 	}
 
 	return fmt.Sprintf("-ERR unknown command '%s'\r\n", cmd)
